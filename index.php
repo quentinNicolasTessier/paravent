6e7c36b1bae0f6f1adfe13927d82df96386b11fd
quentinNicolasTessier <quentin.nicolas.tessier@gmail.com>
2023-11-13 16:13:59 +0100

    v2

--- index.php
+++ index.php
@@ -5,23 +5,36 @@ function beSafe($land,$altitude){
     $count=0;
     $mountain=0;
     if(count($altitudes) > 0) {
-        foreach ($altitudes as $key => $alt) {
-            if ($key < $land) {
-                if ($key == 0) {
-                    $mountain = intval($alt);
+        if(count($altitudes) == $land){
+            foreach ($altitudes as $key => $alt) {
+                if ($key < $land) {
+                    if ($key == 0) {
+                        $mountain = intval($alt);
+                    }
+                    $currentLand = intval($alt);
+                    if ($currentLand < $mountain) {
+                        $count++;
+                    } elseif ($currentLand > $mountain) {
+                        $mountain = $currentLand;
+                    }
                 }
-                $currentLand = intval($alt);
-                if ($currentLand < $mountain) {
-                    $count++;
-                } elseif ($currentLand > $mountain) {
-                    $mountain = $currentLand;
-                }
-            }
 
+            }
         }
+        elseif(count($altitudes) > $land){
+            return "Trop de montagnes par rapport à la surface du continent";
+        }
+        else{
+            return "Pas assez de montagnes par rapport à la surface du continent";
+        }
+
     }
     return $count;
 }
-echo(beSafe(10,"30 27 17 42 29 12 14 41 42 42"));
+echo "Saisir la largeur du continent: ";
+$surface = intval(fgets(STDIN));
+echo "Saisir les altitudes des montagnes: ";
+$montagnes = fgets(STDIN);
+echo(beSafe($surface,$montagnes));
 
 ?>
